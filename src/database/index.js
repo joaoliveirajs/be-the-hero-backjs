9357f151aacc498badd8071b3a94ec73518180df
João Oliveira <joaoliveirajs@gmail.com>
2020-03-25 21:05:42 -0300

    fix: Fixed production env.

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import knex from "knex";
 
 import knexFile from "../../knexfile";
 
-const enviroment = knexFile.production || knexFile.development;
+const enviroment = knexFile.production;
 const connection = knex(enviroment);
 
 export default connection;
